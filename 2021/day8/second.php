51866699986e0f63c3708260fcddcc98ba96f283
Henri Bourcereau <henri.bourcereau@gmail.com>
2021-12-08 18:42:24 +0100

    2021 php day8 second

--- 2021/day8/second.php
+++ 2021/day8/second.php
@@ -1,13 +1,10 @@
 <?php
-$input = parse('./example');
+$input = parse('./input');
 
 $count = array_reduce($input, function($sum, $line){
-  print_r($line);
   $decoder = makeDecoder($line['patterns']);
-  print_r($decoder);
-  echo "test decode edb:".decode($decoder, "edb");
   $decoded = array_map(fn($pattern) => decode($decoder, $pattern), $line['digits']);
-  $digits = join(signalsToDigit($decoded));
+  $digits = signalsToDigit($decoded);
   return $sum + (int)$digits;
 }, 0);
 
@@ -29,29 +26,22 @@ function makeDecoder(array $patterns):array {
   $a_d_g = array_intersect($patterns[3], $patterns[4], $patterns[5]); // 5 segments : 2, 3, 5
   $a_b_f_g = array_intersect($patterns[6], $patterns[7], $patterns[8]); // 6 segments : 0, 6, 9
 
-  $a = array_diff($a_c_f, $c_f);
-  // print_r($a_c_f);
-  // print_r($c_f);
-  // print_r($a);
+  $a = array_diff($a_c_f, $c_f); // ok
   $b = array_diff($a_b_f_g, $a_d_g, $c_f);
-  $c = array_diff($a_c_f, $a_b_f_g);
+  $c = array_diff($a_c_f, $a_b_f_g); // ok
   $d = array_diff($a_d_g, $a_b_f_g);
   $f = array_diff($c_f, $c);
   $g = array_diff($a_b_f_g, $a, $b_c_d_f);
   $e = array_diff($all, $a, $b, $c, $d, $f, $g);
-  // print_r($all);
-  // print_r($a);
-  // print_r($b);
-  // print_r($e);
 
   return [
-    'a' => reset($a),
-    'b' => reset($b),
-    'c' => reset($c),
-    'd' => reset($d),
-    'e' => reset($e),
-    'f' => reset($f),
-    'g' => reset($g),
+    reset($a) => 'a',
+    reset($b) => 'b',
+    reset($c) => 'c',
+    reset($d) => 'd',
+    reset($e) => 'e',
+    reset($f) => 'f',
+    reset($g) => 'g',
   ];
 
 }
